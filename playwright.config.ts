b686876417b39a27c30278fd53c4a14bdeb2a3df
sowjanya.rs@zensar.com <Zen@Git12>
2026-08-13 11:59:05 +0530

    changed env variable names

--- playwright.config.ts
+++ playwright.config.ts
@@ -40,6 +40,7 @@ export default defineConfig({
     ignoreHTTPSErrors: true,
     storageState: AUTH_STATE_PATH, 
     headless:false,
+    video:'retain-on-failure',
      userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36',
     launchOptions: {
       args: [
